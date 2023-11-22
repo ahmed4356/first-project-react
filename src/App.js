963f5c05a549019e72ca6621036aa448f3e82e66
ahmed4356 <alngm.8765@gmail.com>
2023-11-22 02:57:05 +0200

    new

--- src/App.js
+++ src/App.js
@@ -3,8 +3,26 @@
 function App() {
   return (
     <div className="App">
-      <h1>Ahmed AbdElhay</h1>
+      <div className="container">
+        <div className="images">
+          <div className="slid-number" />
+          <img src="/Photo/photo1.jpg" alt="" />
+          <img src="/Photo/photo2.jpg" alt="" />
+          <img src="/Photo/photo3.jpg" alt="" />
+          <img src="/Photo/photo4.jpg" alt="" />
+          <img src="/Photo/photo5.jpg" alt="" />
+          <img src="/Photo/photo6.jpg" alt="" />
+          <img src="/Photo/photo7.jpg" alt="" />
+        </div>
+        <div className="text">
+          <span className="previous">Previous</span>
+          <span className="bullets">
+          </span>
+          <span className="next">Next</span>
+        </div>
+      </div>
     </div>
+
   );
 }
 
